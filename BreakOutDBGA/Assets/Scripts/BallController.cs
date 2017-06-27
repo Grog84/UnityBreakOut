94fff09c0b7b8fb488abeafe0357a910e9555253
FabioBrigidi <fabio.brigidi@gmail.com>
2017-06-27 14:41:47 +0200

    First Commit

---
+++ BreakOutDBGA/Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float ballStartingSpeed = 500.0f;
+
+	private Rigidbody myRigidBody;
+	private bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+
+		myRigidBody = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButton ("Jump") && !isPlaying) {
+
+			transform.parent = null;
+			isPlaying = true;
+			myRigidBody.AddForce (new Vector3 (ballStartingSpeed, ballStartingSpeed, 0));
+
+		}
+		
+	}
+}
---
+++ BreakOutDBGA/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+	public float maxMovementWidth = 8;
+
+	private Transform playerPos;
+	private float xPos;
+
+	// Use this for initialization
+	void Start () {
+
+		playerPos = this.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		playerPos.position = new Vector3 (Mathf.Clamp (xPos, -maxMovementWidth, maxMovementWidth), playerPos.position.y, playerPos.position.z);
+
+	}
+}
