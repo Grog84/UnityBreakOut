f188270481f381a5dd2301c517f523235d3517e2
FabioBrigidi <fabio.brigidi@gmail.com>
2017-06-27 15:31:47 +0200

    Bricks are disappearing

---
+++ BreakOutDBGA/Assets/Scripts/BrickController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickController : MonoBehaviour {
+
+	private bool isHit = false;
+
+	void LateUpdate () {
+		if (isHit == true) {
+			this.gameObject.SetActive (false);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		setHitStatus ();
+	}
+
+	void setHitStatus(){
+		isHit = true;
+	}
+}
